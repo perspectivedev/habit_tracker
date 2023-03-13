710c1bd3db7a23ba306490d66f51be0170c596ac
Marvis Knight <knightmar78@gmail.com>
2023-03-12 20:08:15 -0700

    initial commit

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+// const mongoose = require('mongoose');
+
+
+
+app.listen(3000, () => {
+    console.log('Yay! The Server is working.');
+});
