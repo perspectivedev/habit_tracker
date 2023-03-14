50570dfb5367af4924b856fb4348794f1e644258
Marvis Knight <knightmar78@gmail.com>
2023-03-14 09:02:41 -0700

    node modules installed

--- server/index.js
+++ server/index.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const app = express();
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const UserModel = require('./models/Users');
 
+mongoose.connect(
+    'mongodb+srv://My1habit:$Let4820$@cluster0.dwelmno.mongodb.net/habits?retryWrites=true&w=majority'
+);
+
+
+app.get('/getUsers', async function(req, res) {
+    await UserModel.find({}, (err, result) => {
+        if (err){
+            res.json(err);
+        } else {
+            res.json(result);
+        }
+    })
+});
 
 
 app.listen(3000, () => {
