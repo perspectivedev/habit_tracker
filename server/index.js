48433d13ae9ba7ea2775acf955a8eb5ef50c9f8e
Marvis Knight <knightmar78@gmail.com>
2023-03-16 22:19:26 -0700

    brought in react-create-app

--- server/index.js
+++ server/index.js
@@ -1,22 +1,36 @@
-const express = require('express');
+import express, { json } from 'express';
 const app = express();
-const mongoose = require('mongoose');
-const UserModel = require('./models/Users');
+import { connect } from 'mongoose';
+import UserModel, { find } from './models/Users';
+import cors from 'cors';
 
-mongoose.connect(
-    'mongodb+srv://My1habit:$Let4820$@cluster0.dwelmno.mongodb.net/habits?retryWrites=true&w=majority'
-);
-app.get('./getUsers', async (req, res) => {
-    UserModel.find({}, (err, result) => {
+app.use(json());
+app.use(cors());
+
+connect(
+      'mongodb+srv://My1habit:$Let4820$@cluster0.dwelmno.mongodb.net/habits?retryWrites=true&w=majority'
+ );
+
+
+app.get('./getUsers', (req, res) =>  {
+    find({}, (err, result) => {
         if(err){
             res.json(err);
         } else {
             res.json(result);
         }
-    
-})
+    })
 });
 
+
+app.post('./createUser', async (req, res) => {
+    const user = req.body;
+    const newUser = new UserModel(user);
+    await newUser.save();
+
+
+    res.json(user);
+})
 
 app.listen(3000, () => {
     console.log('Yay! The Server is working.');
